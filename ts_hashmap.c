579f3f530745154f581495d1281266278e269738
JDB <legodude17@users.noreply.github.com>
2024-03-27 10:16:09 -0700

    Complete Assignment

--- ts_hashmap.c
+++ ts_hashmap.c
@@ -6,14 +6,29 @@
 #include "ts_hashmap.h"
 
 /**
- * Creates a new thread-safe hashmap. 
+ * Creates a new thread-safe hashmap.
  *
  * @param capacity initial capacity of the hashmap.
  * @return a pointer to a new thread-safe hashmap.
  */
-ts_hashmap_t *initmap(int capacity) {
-  // TODO
-  return NULL;
+ts_hashmap_t *initmap(int capacity)
+{
+  ts_hashmap_t *map = (ts_hashmap_t *)malloc(sizeof(ts_hashmap_t));
+  map->capacity = capacity;
+  map->numOps = 0;
+  map->size = 0;
+  // Use one lock per linked list, plus one for the numOps and size
+  map->table = (ts_entry_t **)malloc(capacity * sizeof(ts_entry_t *));
+  map->entryLocks = (pthread_mutex_t **)malloc(capacity * sizeof(pthread_mutex_t *));
+  for (int i = 0; i < capacity; i++)
+  {
+    map->table[i] = NULL;
+    map->entryLocks[i] = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(map->entryLocks[i], NULL);
+  }
+  map->propsLock = (pthread_spinlock_t *)malloc(sizeof(pthread_spinlock_t));
+  pthread_spin_init(map->propsLock, PTHREAD_PROCESS_PRIVATE);
+  return map;
 }
 
 /**
@@ -22,8 +37,27 @@ ts_hashmap_t *initmap(int capacity) {
  * @param key a key to search
  * @return the value associated with the given key, or INT_MAX if key not found
  */
-int get(ts_hashmap_t *map, int key) {
-  // TODO
+int get(ts_hashmap_t *map, int key)
+{
+  // Need to lock when we increment numOps
+  pthread_spin_lock(map->propsLock);
+  map->numOps++;
+  pthread_spin_unlock(map->propsLock);
+  unsigned int index = ((unsigned int)key) % map->capacity;
+  pthread_mutex_lock(map->entryLocks[index]);
+  ts_entry_t *entry = map->table[index];
+  // Traverse the linked list, looking for the key
+  while (entry != NULL)
+  {
+    if (entry->key == key)
+    {
+      int value = entry->value;
+      pthread_mutex_unlock(map->entryLocks[index]);
+      return value;
+    }
+    entry = entry->next;
+  }
+  pthread_mutex_unlock(map->entryLocks[index]);
   return INT_MAX;
 }
 
@@ -34,8 +68,35 @@ int get(ts_hashmap_t *map, int key) {
  * @param value a value
  * @return old associated value, or INT_MAX if the key was new
  */
-int put(ts_hashmap_t *map, int key, int value) {
-  // TODO
+int put(ts_hashmap_t *map, int key, int value)
+{
+  unsigned int index = ((unsigned int)key) % map->capacity;
+  pthread_mutex_lock(map->entryLocks[index]);
+  ts_entry_t *entry = map->table[index];
+  while (entry != NULL)
+  {
+    if (entry->key == key)
+    {
+      int oldValue = entry->value;
+      entry->value = value;
+      pthread_mutex_unlock(map->entryLocks[index]);
+      pthread_spin_lock(map->propsLock);
+      map->numOps++;
+      pthread_spin_unlock(map->propsLock);
+      return oldValue;
+    }
+    entry = entry->next;
+  }
+  entry = (ts_entry_t *)malloc(sizeof(ts_entry_t));
+  entry->key = key;
+  entry->value = value;
+  entry->next = map->table[index];
+  map->table[index] = entry;
+  pthread_mutex_unlock(map->entryLocks[index]);
+  pthread_spin_lock(map->propsLock);
+  map->numOps++;
+  map->size++;
+  pthread_spin_unlock(map->propsLock);
   return INT_MAX;
 }
 
@@ -45,20 +106,51 @@ int put(ts_hashmap_t *map, int key, int value) {
  * @param key a key to search
  * @return the value associated with the given key, or INT_MAX if key not found
  */
-int del(ts_hashmap_t *map, int key) {
-  // TODO
+int del(ts_hashmap_t *map, int key)
+{
+  pthread_spin_lock(map->propsLock);
+  map->numOps++;
+  map->size--;
+  pthread_spin_unlock(map->propsLock);
+  unsigned int index = ((unsigned int)key) % map->capacity;
+  pthread_mutex_lock(map->entryLocks[index]);
+  ts_entry_t *entry = map->table[index];
+  ts_entry_t *prevEntry = NULL;
+  while (entry != NULL)
+  {
+    if (entry->key == key)
+    {
+      if (prevEntry == NULL)
+      {
+        map->table[index] = entry->next;
+      }
+      else
+      {
+        prevEntry->next = entry->next;
+      }
+      int value = entry->value;
+      free(entry);
+      pthread_mutex_unlock(map->entryLocks[index]);
+      return value;
+    }
+    prevEntry = entry;
+    entry = entry->next;
+  }
+  pthread_mutex_unlock(map->entryLocks[index]);
   return INT_MAX;
 }
-
 
 /**
  * Prints the contents of the map (given)
  */
-void printmap(ts_hashmap_t *map) {
-  for (int i = 0; i < map->capacity; i++) {
+void printmap(ts_hashmap_t *map)
+{
+  for (int i = 0; i < map->capacity; i++)
+  {
     printf("[%d] -> ", i);
     ts_entry_t *entry = map->table[i];
-    while (entry != NULL) {
+    while (entry != NULL)
+    {
       printf("(%d,%d)", entry->key, entry->value);
       if (entry->next != NULL)
         printf(" -> ");
@@ -72,8 +164,23 @@ void printmap(ts_hashmap_t *map) {
  * Free up the space allocated for hashmap
  * @param map a pointer to the map
  */
-void freeMap(ts_hashmap_t *map) {
-  // TODO: iterate through each list, free up all nodes
-  // TODO: free the hash table
-  // TODO: destroy locks
+void freeMap(ts_hashmap_t *map)
+{
+  for (int i = 0; i < map->capacity; i++)
+  {
+    ts_entry_t *entry = map->table[i];
+    while (entry != NULL)
+    {
+      ts_entry_t *next = entry->next;
+      free(entry);
+      entry = next;
+    }
+    pthread_mutex_destroy(map->entryLocks[i]);
+    free(map->entryLocks[i]);
+  }
+  free(map->table);
+  free(map->entryLocks);
+  pthread_spin_destroy(map->propsLock);
+  free(map->propsLock);
+  free(map);
 }
--- ts_hashmap.h
+++ ts_hashmap.h
@@ -2,26 +2,30 @@
 
 // A hashmap entry stores the key, value
 // and a pointer to the next entry
-typedef struct ts_entry_t {
+typedef struct ts_entry_t
+{
    int key;
    int value;
    struct ts_entry_t *next;
 } ts_entry_t;
 
 // A hashmap contains an array of pointers to entries,
-// the capacity of the array, the size (number of entries stored), 
+// the capacity of the array, the size (number of entries stored),
 // and the number of operations that it has run.
-typedef struct ts_hashmap_t {
+typedef struct ts_hashmap_t
+{
    ts_entry_t **table;
+   pthread_mutex_t **entryLocks;
    int numOps;
    int capacity;
    int size;
+   pthread_spinlock_t *propsLock;
 } ts_hashmap_t;
 
 // function declarations
 ts_hashmap_t *initmap(int);
-int get(ts_hashmap_t*, int);
-int put(ts_hashmap_t*, int, int);
-int del(ts_hashmap_t*, int);
-void printmap(ts_hashmap_t*);
-void freeMap(ts_hashmap_t*);
+int get(ts_hashmap_t *, int);
+int put(ts_hashmap_t *, int, int);
+int del(ts_hashmap_t *, int);
+void printmap(ts_hashmap_t *);
+void freeMap(ts_hashmap_t *);
